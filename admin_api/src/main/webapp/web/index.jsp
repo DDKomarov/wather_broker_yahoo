64c639156f8c7b1dd10233f25bbc558c95283d94
Komarov Dmitriy <DDKomarov@bell-main.bellintegrator.ru>
2019-02-19 16:29:29 +0400

    first commit

---
+++ admin_api/src/main/webapp/web/index.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: DDKomarov
+  Date: 19.02.2019
+  Time: 13:22
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<div align="center">
+    <input>
+    <button id="submit" name="submit">submit</button>
+</div>
+</body>
+</html>
---
+++ db_service/src/main/java/Weather.java
@@ -0,0 +1,53 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Weather {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String city;
+    private String region;
+    private String country;
+    private String condition;
+    private String temp;
+    private String chill;
+    private String humidity;
+
+    public Weather() {}
+
+    public Weather(String city, String region, String country, String condition, String temp, String chill, String humidity) {
+        this.city = city;
+        this.region = region;
+        this.country = country;
+        this.condition = condition;
+        this.temp = temp;
+        this.chill = chill;
+        this.humidity = humidity;
+    }
+
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+
+    public String getRegion() {	return region; }
+    public void setRegion(String region) { this.region = region; }
+
+    public String getCountry() { return country; }
+    public void setCountry(String country) { this.country = country; }
+
+    public String getCondition() { return condition; }
+    public void setCondition(String condition) { this.condition = condition; }
+
+    public String getTemp() { return temp; }
+    public void setTemp(String temp) {	this.temp = temp; }
+
+    public String getChill() { return chill; }
+    public void setChill(String chill) { this.chill = chill; }
+
+    public String getHumidity() { return humidity; }
+    public void setHumidity(String humidity) { this.humidity = humidity; }
+
+}
